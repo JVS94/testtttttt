bce39e205b3fb58fd716bb9ad8b293564c0b4a81
JVS94 <44021373+JVS94@users.noreply.github.com>
2020-11-10 13:32:33 +0800

    Update AAA.java

--- src/aaa/AAA.java
+++ src/aaa/AAA.java
@@ -2,8 +2,6 @@ package aaa;
 
 public class AAA {
 	public static void main(String[] args) {
-		System.out.println(666);
-		System.out.println(777);
-		System.out.println(888);
+		System.out.println(2);
 	}
 }
